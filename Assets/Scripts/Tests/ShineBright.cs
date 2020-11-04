5b74c14fec0fe4507b92bc5769ac196ac66382d6
ffsjkorn <jkorn2324swagg@gmail.com>
2020-11-03 19:54:51 -0800

    Finally i can push my changes

---
+++ Assets/Scripts/Tests/ShineBright.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CyberMonk.Tests
+{
+    /// <summary>
+    /// The class for the shine bright monobehviour.
+    /// This is not included in the game it's just
+    /// used as a test object to sync beats.
+    /// </summary>
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class ShineBright : MonoBehaviour
+    {
+
+        private SpriteRenderer _renderer;
+
+        /// <summary>
+        /// Called when the object has started.
+        /// </summary>
+        private void Start()
+        {
+            this.HookEvents();
+            this._renderer = this.GetComponent<SpriteRenderer>();
+        }
+
+        /// <summary>
+        /// Called when the object is enabled.
+        /// </summary>
+        private void OnEnable()
+        {
+            this.HookEvents();
+        }
+
+        /// <summary>
+        /// Called to hook the events that this script uses.
+        /// </summary>
+        private void HookEvents()
+        {
+            if (Game.Conductor.Instance != null)
+            {
+                Game.Conductor.Instance.DownBeatEvent += OnDownBeat;
+            }
+        }
+
+        /// <summary>
+        /// Called when the object is disabled.
+        /// </summary>
+        private void OnDisable()
+        {
+            if(Game.Conductor.Instance != null)
+            {
+                Game.Conductor.Instance.DownBeatEvent -= OnDownBeat;
+            }
+        }
+
+        /// <summary>
+        /// Called on the down beat.
+        /// </summary>
+        private void OnDownBeat()
+        {
+            Debug.Log("beat down");
+            this._renderer.enabled = !this._renderer.enabled;
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/Attributes/ReadOnlyDrawer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace CyberMonk.Utils.Attributes
+{
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        /// <summary>
+        /// Unity method for drawing GUI in Editor
+        /// </summary>
+        /// <param name="position">The position of the property..</param>
+        /// <param name="property">The serialized property..</param>
+        /// <param name="label">The GUI Content label.</param>
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            bool prevValue = GUI.enabled;
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = prevValue;
+        }
+    }
+}
