ec635ff1a4c1cbeffc7c6f68b1e54438c078a200
ffsjkorn <jkorn2324swagg@gmail.com>
2020-11-08 00:22:51 -0800

    Some monkey stuff

--- Assets/Scripts/Game/Moonkey/MoonkeyController.cs
+++ Assets/Scripts/Game/Moonkey/MoonkeyController.cs
@@ -6,29 +6,34 @@ using UnityEngine;
 
 namespace CyberMonk.Game.Moonkey 
 {
+    /// <summary>
+    /// The Monkey Controller class definition.
+    /// </summary>
     public class MoonkeyController
     {
         #region fields
+        
         private readonly MoonkeyComponent _component;
         private readonly MoonkeySettings _settings;
         private readonly MoonkeyMovementController _movementController;
-        
 
         #endregion
 
         #region properties
+        
         public MoonkeyComponent Component
             => this._component;
+        
         public MoonkeySettings Settings
             => this._settings;
+
         public virtual MoonkeyMovementController MovementController
             => this._movementController;
 
-
         #endregion
 
-
         #region constructor
+
         public MoonkeyController(MoonkeyComponent component, MoonkeySettings settings )
         {
             Debug.Log("constructing controller class");
@@ -41,8 +46,44 @@ namespace CyberMonk.Game.Moonkey
         #endregion
 
 
+        #region methods
+
+        /// <summary>
+        /// Called to hook the events.
+        /// </summary>
+        public void HookEvents()
+        {
+            // TODO: hooks the events
+        }
+
+        /// <summary>
+        /// Called to unhook the events.
+        /// </summary>
+        public void UnhookEvents()
+        {
+            // TODO: Unhooks the events.
+        }
+
+        /// <summary>
+        /// Updates the monkey.
+        /// </summary>
+        public void Update()
+        {
+            this._movementController?.Update();
+        }
+
+        /// <summary>
+        /// Updates the physics of the monkey.
+        /// </summary>
+        public void PhysicsUpdate()
+        {
+            this._movementController?.PhysicsUpdate();
+        }
+
+        #endregion
+
         #region static_methods
-        
+
 
 
         #endregion
