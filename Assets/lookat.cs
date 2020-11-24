0bacfba8515379f65b9b7a19a00fc94f86acbc16
jeronartest <artestjeron@gmail.com>
2020-11-24 02:40:19 -0800

    level experimentations

---
+++ Assets/lookat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookat : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.transform, Vector2.up);
+    }
+}
