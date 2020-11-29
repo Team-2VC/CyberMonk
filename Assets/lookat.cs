4fb683f3c9cd69221f24f3c05018d2dc029bd617
ffsjkorn <jkorn2324swagg@gmail.com>
2020-11-28 18:03:33 -0800

    Fixed rotation of spotlight

--- Assets/lookat.cs
+++ Assets/lookat.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class lookat : MonoBehaviour
 {
-    public GameObject target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private GameObject target;
+    
+    /// <summary>
+    /// Update is called once per frame.
+    /// </summary>
+    private void Update()
     {
-        transform.LookAt(target.transform, Vector2.up);
+        if(this.target == null)
+        {
+            return;
+        }
+
+        Vector3 ourPosition = this.transform.position;
+        Vector3 targetPosition = this.target.transform.position;
+
+        Vector2 direction = new Vector2(targetPosition.x - ourPosition.x, targetPosition.y - ourPosition.y);
+        float rotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+        this.transform.eulerAngles = new Vector3(0, 0, rotation);
     }
 }
