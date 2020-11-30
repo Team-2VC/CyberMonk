806614e6039db7d0cf774ae6879a67370d5f5d36
jeronartest <artestjeron@gmail.com>
2020-11-29 20:18:11 -0800

    light pulses, kinda messy :sob:

---
+++ Assets/Scripts/Game/Lights/BeatFlash.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CyberMonk.Game.Lights
+{
+    public class BeatFlash : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Utils.References.IntegerReference beatCounter;
+
+        [SerializeField]
+        private Utils.Events.GameEvent beatDownEvent;
+
+        [SerializeField]
+        private bool upBeat = true;
+
+        private Animator animator;
+
+        private void Start()
+        {
+            animator = this.GetComponent<Animator>();
+        }
+
+        private void OnEnable()
+        {
+            HookEvent();
+        }
+
+        private void OnDisable()
+        {
+            UnhookEvent();
+        }
+
+        private void HookEvent()
+        {
+            this.beatDownEvent += this.OnDownBeat;
+        }
+
+        private void UnhookEvent()
+        {
+            this.beatDownEvent -= this.OnDownBeat;
+        }
+
+        private void OnDownBeat()
+        {
+            int x = upBeat ? 1 : 0;
+            if(beatCounter.Value % 2 == x)
+                this.animator.Play("Flash");
+        }
+
+
+    }
+}
+
