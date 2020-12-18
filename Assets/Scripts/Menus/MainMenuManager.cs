a383e37bb6e5614efa041d05441152017f37e81b
ffsjkorn <jkorn2324swagg@gmail.com>
2020-12-17 16:41:12 -0800

    Transitions & menu updates

--- Assets/Scripts/Menus/MainMenuManager.cs
+++ Assets/Scripts/Menus/MainMenuManager.cs
@@ -5,17 +5,78 @@ using UnityEngine.SceneManagement;
 
 namespace CyberMonk.Menus
 {
+    public struct TransitionData
+    {
+        public object data;
+        public System.Action<object> callable;
+    }
+
+    [System.Serializable]
+    public struct AnimationTransitions
+    {
+        [SerializeField]
+        private string playGameTransition;
+
+        public string PlayGameTransition
+            => this.playGameTransition;
+    }
+
     /// <summary>
     /// The Main Menu implementation.
     /// </summary>
+    [RequireComponent(typeof(Animator))]
     public class MainMenuManager : MonoBehaviour
     {
+
+        [SerializeField]
+        private AnimationTransitions animationTransitions;
+        private Animator _animator;
+
+        private TransitionData? _currentTransition = null;
+
+        protected bool IsInTransition
+            => this._currentTransition.HasValue;
+
+        /// <summary>
+        /// Called when the main menu has started.
+        /// </summary>
+        private void Start()
+        {
+            this._animator = this.GetComponent<Animator>();
+        }
+
         /// <summary>
         /// Called when the play button is selected.
         /// </summary>
         public void OnPlaySelected(string level)
         {
-            SceneManager.LoadScene(level);
+            if(this.IsInTransition)
+            {
+                return;
+            }
+
+            this._animator.Play(this.animationTransitions.PlayGameTransition);
+            this.SetTransitionData((object a) =>
+                {
+                    if(a is string)
+                    {
+                        // TODO: Load instructions scene??
+                        SceneManager.LoadSceneAsync((string)a);
+                    }
+                }, level);
+        }
+
+        /// <summary>
+        /// Called when the credits button was selected.
+        /// </summary>
+        public void OnCreditsSelected()
+        {
+            if (this.IsInTransition)
+            {
+                return;
+            }
+
+            // TODO: Implementation
         }
 
         /// <summary>
@@ -23,7 +84,41 @@ namespace CyberMonk.Menus
         /// </summary>
         public void OnQuitSelected()
         {
+            if(this.IsInTransition)
+            {
+                return;
+            }
+
             Application.Quit();
+        }
+
+
+        /// <summary>
+        /// Sets the transition data.
+        /// </summary>
+        /// <param name="callable">The callable.</param>
+        /// <param name="data">The data used by the transition.</param>
+        private void SetTransitionData(System.Action<object> callable, object data = null)
+        {
+            TransitionData dat = new TransitionData();
+            dat.data = data;
+            dat.callable = callable;
+            this._currentTransition = dat;
+        }
+
+        /// <summary>
+        /// Called when the animation transition has finished.
+        /// </summary>
+        private void OnTransitionFinished()
+        {
+            if(!this.IsInTransition)
+            {
+                return;
+            }
+
+            TransitionData data = this._currentTransition.Value;
+            data.callable(data.data);
+            this._currentTransition = null;
         }
     }
 }
